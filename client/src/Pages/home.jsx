cd8723642b8a17dbc036f03912f5bffb5d5f0a25
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-07-01 12:25:52 +0530

    this is temp

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -5,8 +5,11 @@ import {Steps} from "./Components/home/steps";
 import {PopularJobs} from "./Components/home/popularJobs";
 import {News} from "./Components/home/news";
 import {Footer} from "./Components/home/footer";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+AOS.init();
 export function Home() {
+
     return (<>
         <Navbar/>
         <LandingPage/>
