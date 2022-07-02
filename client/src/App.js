477e91f30198041d34583e409e4456d387b2dd27
aaditya010202 <aadibhatnagar535@gmail.com>
2022-07-03 00:26:32 +0530

    login again

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import {Home} from './Pages/home';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Home } from "./Pages/home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import {Compare} from "./Pages/compare";
-import {Login} from "./Pages/login";
+import { Compare } from "./Pages/compare";
+import { Login } from "./Pages/login";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/compare" element={<Compare/>}/>
-                <Route path="/login" element={<Login/>}/>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/compare" element={<Compare />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
