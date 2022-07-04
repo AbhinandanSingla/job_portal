130adc860538b1028db6a4fbae9fdaf472f98fa5
Ananya jain <ajain5_be20@thapar.edu>
2022-07-04 22:55:20 +0530

    hello

--- client/src/Pages/Components/home/Navbar.jsx
+++ client/src/Pages/Components/home/Navbar.jsx
@@ -1,20 +1,24 @@
-import logo from '../../../Assets/Images/landingPage/logo.jpeg';
+import logo from "../../../Assets/Images/landingPage/logo.jpeg";
 import homeStyle from "../../../Assets/styles/style.module.css";
+// import {Link} from "react-router-dom";
 
 export function Navbar() {
-    return (<section className={homeStyle.navbar}>
-            <div className={homeStyle.max_width}>
-                <div className={homeStyle.nav_logo}>
-                    <img src={logo} alt="Logo"/>
-                </div>
-                <ul className={homeStyle.navsContainer}>
-                    <li className={homeStyle.navs}>Home</li>
-                    <li className={homeStyle.navs}><a href="/compare"> Category</a></li>
-                    <li className={homeStyle.navs}>Post a job</li>
-                    <li className={homeStyle.navs}>Contact</li>
-                    <li className={homeStyle.btn}>Create Account</li>
-                </ul>
-            </div>
-        </section>
-    );
+  return (
+    <section className={homeStyle.navbar}>
+      <div className={homeStyle.max_width}>
+        <div className={homeStyle.nav_logo}>
+          <img src={logo} alt="Logo" />
+        </div>
+        <ul className={homeStyle.navsContainer}>
+          <li className={homeStyle.navs}>Home</li>
+          <li className={homeStyle.navs}>
+            <a href="/compare"> Category</a>
+          </li>
+          <li className={homeStyle.navs}>Post a job</li>
+          <li className={homeStyle.navs}>Contact</li>
+          <li className={homeStyle.btn}>Create Account</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
