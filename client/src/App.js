3d10daba4e6d8c7a4cc86e3b66707af8ac049a4b
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-07-08 15:16:13 +0530

    compare page

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/compare" element={<Compare />} />
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
