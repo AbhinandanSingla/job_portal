993da4a78475ad44a6eeca0f1f6ecf39362520cf
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-08-06 23:58:47 +0530

    graphQl

--- server/models/Schema.js
+++ server/models/Schema.js
@@ -1,36 +1,33 @@
 import pkg from 'graphql';
 import {getJobs} from "../controllers/admin.js";
+import {userType} from "./Queries.js";
 
 const {GraphQLInt, GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString} = pkg;
 
 
-const userType = new GraphQLObjectType({
-    name: "user",
-    fields: () => ({
-        _id: {
-            type: GraphQLID
-        }
-    })
-})
-
 const RootQuery = new GraphQLObjectType({
     name: "name",
     fields: {
-        codeimprove: {
+        jobList: {
             type: new GraphQLList(userType),
             resolve(parent, args) {
-                return [{
-                    id: 12, name: "hmmm", email: 'fwff'
-                }];
-            }
-        },
-        userList: {
-            type: new GraphQLList(userType),
-            resolve(parent, args) {
-                let a = getJobs()
-                return a;
+                return getJobs();
             }
         }
     }
 })
-export default new GraphQLSchema({query: RootQuery})
+const userList = {
+    type: userType,
+    args: {
+        name: {type: GraphQLString,},
+        email: {type: GraphQLString},
+        gender: {type: GraphQLString}
+    }, resolve(parent, args) {
+        console.log(args)
+        return args
+    }
+}
+const Mutation = new GraphQLObjectType({
+    name: 'mutation', fields: {createUser: userList}
+})
+export default new GraphQLSchema({query: RootQuery, mutation: Mutation})
