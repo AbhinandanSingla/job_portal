8faa247647a39405c7165d50094c3bf0510ea8e7
aaditya010202 <aadibhatnagar535@gmail.com>
2022-08-10 23:51:35 +0530

    abc

--- client/src/Pages/Components/admin_aaditya/jobs.jsx
+++ client/src/Pages/Components/admin_aaditya/jobs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import aadityaModule from "./aaditya.module.css";
+import aadityaModule from "../../../Assets/styles/aaditya.module.css";
 export const Jobs = () => {
   return (
     <div className={aadityaModule.jobs}>
@@ -13,9 +13,11 @@ export const Jobs = () => {
         <h2>Job categories</h2>
         <div className={aadityaModule.jobCat}>
           <div>
+            {/* <div className={aadityaModule.svgClass}> */}
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
               <path d="M224 263.3C224.2 233.3 238.4 205.2 262.4 187.2L499.1 9.605C517.7-4.353 543.6-2.965 560.7 12.9C577.7 28.76 580.8 54.54 568.2 74.07L406.5 324.1C391.3 347.7 366.6 363.2 339.3 367.1L224 263.3zM320 400C320 461.9 269.9 512 208 512H64C46.33 512 32 497.7 32 480C32 462.3 46.33 448 64 448H68.81C86.44 448 98.4 429.1 96.59 411.6C96.2 407.8 96 403.9 96 400C96 339.6 143.9 290.3 203.7 288.1L319.8 392.5C319.9 394.1 320 397.5 320 400V400z" />
             </svg>
+            {/* </div> */}
             <p> Design</p>
           </div>
           <div>
