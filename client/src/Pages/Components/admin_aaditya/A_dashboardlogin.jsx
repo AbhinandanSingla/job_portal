519069fc72a08e0782eccb4e4a965ae42c39ce3e
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-08-12 18:07:04 +0530

    compare page completed

---
+++ client/src/Pages/Components/admin_aaditya/A_dashboardlogin.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react'
+import logo from "../../../Assets/Images/common/logo.svg";
+import aadityaModule from "./aaditya.module.css";
+
+export const Dashboardlogin = () => {
+  return (
+    <div>
+      <div className={aadityaModule.phto}>
+      <div className={aadityaModule.rr1}>
+
+</div>
+<div className={aadityaModule.rr2}>
+
+</div>
+<img src={logo} alt="" />
+      <h2>Rozgaar.com</h2>
+      <h1>Sign in to your dashboard</h1>
+      <p>Enter your email address and password to continue</p>
+      </div>
+      <div >
+        <div className={aadityaModule.ob} >
+          <p>admin@company.com</p>
+        </div>
+
+        <div className={aadityaModule.ob1}>
+          <p>* * * * * * *</p>
+        </div>
+
+      </div>
+      <div className={aadityaModule.bte}>
+        <p>Sign In kardodd</p>
+      </div>
+
+    </div>
+  )
+}
