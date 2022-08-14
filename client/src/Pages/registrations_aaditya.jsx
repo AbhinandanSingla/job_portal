1fb3c317d3c48ea270f1091c5dce8625c8fe7dd3
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-08-14 21:38:20 +0530

    some updates

---
+++ client/src/Pages/registrations_aaditya.jsx
@@ -0,0 +1,159 @@
+import React from "react";
+import {Navbar} from "./Components/admin_aaditya/navbar_aaditya";
+import aadityaModule from "../Assets/styles/aaditya.module.css";
+
+export const Registrations = () => {
+    return <div className={aadityaModule.cl}>
+        <Navbar/>
+        <div className={aadityaModule.ot}>
+            <h1> Total Applicants:19</h1>
+            <div className={aadityaModule.sb}>
+                <p>Search Applicants</p>
+            </div>
+            <div className={aadityaModule.sb2}>
+                <p>Filter</p>
+            </div>
+        </div>
+
+        <div className={aadityaModule.lt}>
+
+            <p1>Full Name</p1>
+            <p2>Company Name</p2>
+            <p3>Applied Date</p3>
+            <p4>Email</p4>
+            <p5>Action</p5>
+
+        </div>
+
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+        <div className={aadityaModule.ll}>
+
+            <div className={aadityaModule.cs}>
+
+            </div>
+            <p>Company Name</p>
+            <div className={aadityaModule.ct}>
+                <p>Technical</p>
+            </div>
+            <p1>13 August 2022</p1>
+            <p2>Email.com</p2>
+            <button className={aadityaModule.gb}>Accept</button>
+            <button className={aadityaModule.rb}>Reject</button>
+        </div>
+
+    </div>;
+
+};
